0363b9f598d2d3d775636053463920f9c4e4eb1f
benPedahtsour <stgameliel@yahoo.fr>
2025-11-19 15:36:14 +0100

    Phase 8: ORM (tri des enregistrements)

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -13,16 +13,24 @@ use Symfony\Component\Routing\Annotation\Route;
  * @author stgam
  */
 class VoyagesController extends AbstractController{
-    #[Route('/voyages', name: 'voyages')]
-    public function index() : Response {
-        $visites = $this->repository->findAll();
-        return $this->render("pages/voyages.html.twig", ['visites' => $visites]);
-    }
-    /**
+        /**
      * 
      * @var VisiteRepository
      */
     private $repository;
+    
+    #[Route('/voyages', name: 'voyages')]
+    public function index() : Response {
+        $visites = $this->repository->findAllOrderBy('datecreation', 'DESC');
+        return $this->render("pages/voyages.html.twig", ['visites' => $visites]);
+    }
+    
+    #[Route('/voyages/tri/{champ}/{ordre}', name: 'voyages.sort')]
+    public function sort($champ, $ordre): Response{
+        $visites = $this->repository->findAllOrderBy($champ, $ordre);
+        return $this->render("pages/voyages.html.twig", ['visites' => $visites]);
+    }
+    
     /**
      * 
      * @param VisiteRepository $repository
